ec9eed08a0fc93333ebd548e2761640f14869fa2
loganmurphy <loganmurphy1984@gmail.com>
2017-12-10 00:15:52 -0600

    updated Procfile

--- yoda_speak/views.py
+++ yoda_speak/views.py
@@ -37,8 +37,8 @@ def google_endpoint (request):
     #     return start_conversation(request)
 
     requested = request.data['inputs'][0]['rawInputs'][0]['query']
-    if request.data:
-    # if request.data['inputs'][0]['intent'] == 'actions.intent.MAIN':
+
+    if request.data['inputs'][0]['intent'] == 'actions.intent.MAIN':
         return start_conversation(request)
     else:
         if (requested.lower() == 'what can I say' or 'options' in requested.lower()):
