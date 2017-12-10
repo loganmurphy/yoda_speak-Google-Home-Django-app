1e2c3caae1d04fffe4f90e1b5a08968fecbabe17
loganmurphy <loganmurphy1984@gmail.com>
2017-12-10 00:30:04 -0600

    updated Procfile

--- google_yoda/settings/production.py
+++ google_yoda/settings/production.py
@@ -2,3 +2,10 @@
 DEBUG = False
 
 from google_yoda.settings.base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'postgres://rdjlskrdbncwyp:d5197ce71d34d89d5ac1e2dc6a5278c6e92cba85c3255c708ffd8181ecb4c1a9@ec2-107-22-160-199.compute-1.amazonaws.com:5432/d154lfgjapcu2v',
+        'NAME': 'yoda',
+    }
+}
