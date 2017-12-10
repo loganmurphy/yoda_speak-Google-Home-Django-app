dafc7fa298fee3016bd3f5360434f06798db83aa
loganmurphy <loganmurphy1984@gmail.com>
2017-12-10 00:51:35 -0600

    updated Procfile

--- google_yoda/settings/production.py
+++ google_yoda/settings/production.py
@@ -2,6 +2,14 @@
 DEBUG = False
 
 from google_yoda.settings.base import *
-import dj_database_url
 
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "name",
+        "USER": "postgres",
+        "PASSWORD": "pass",
+        "HOST": "postgres://rdjlskrdbncwyp:d5197ce71d34d89d5ac1e2dc6a5278c6e92cba85c3255c708ffd8181ecb4c1a9@ec2-107-22-160-199.compute-1.amazonaws.com:5432/d154lfgjapcu2v",
+        "PORT": 5432,
+    }
+}
