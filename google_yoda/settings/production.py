52b6ceea36431ff13eccbfa732b0e25897bfd160
loganmurphy <loganmurphy1984@gmail.com>
2017-12-10 01:09:58 -0600

    updated Procfile

--- google_yoda/settings/production.py
+++ google_yoda/settings/production.py
@@ -6,10 +6,9 @@ from google_yoda.settings.base import *
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "name",
-        "USER": "postgres",
-        "PASSWORD": "pass",
-        "HOST": "postgres://rdjlskrdbncwyp:d5197ce71d34d89d5ac1e2dc6a5278c6e92cba85c3255c708ffd8181ecb4c1a9@ec2-107-22-160-199.compute-1.amazonaws.com:5432/d154lfgjapcu2v",
+        "DATABASE": "d154lfgjapcu2v",
+        "DIALECT": "postgres",
+        "HOST": "ec2-107-22-160-199.compute-1.amazonaws.com",
         "PORT": 5432,
     }
 }
