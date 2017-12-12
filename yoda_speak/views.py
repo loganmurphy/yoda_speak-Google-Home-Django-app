9ef00fd9067c5cfb10b4a41e7fa9383b78da2678
loganmurphy <loganmurphy1984@gmail.com>
2017-12-12 10:55:21 -0600

    updated end_conversation

--- yoda_speak/views.py
+++ yoda_speak/views.py
@@ -70,7 +70,7 @@ def google_endpoint (request):
             return get_phrase(request)
 
 @api_view(['GET'])
-def yoda_get:
+def yodaGet:
     yoda_phrase = YodaPhrase.objects.order_by('-created')[:10]
     return yoda_phrase
 
