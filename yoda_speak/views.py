fe3d03c5ea20b4265ae9afe6bd8688599dc1f412
loganmurphy <loganmurphy1984@gmail.com>
2017-12-12 12:25:05 -0600

    updated end_conversation

--- yoda_speak/views.py
+++ yoda_speak/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from yoda_speak.models import YodaPhrase, Padawan
 
 from django.utils import timezone
